094238afbf200c8e2ac4def0679d23798338ddea
Athul-Sagar <athulsagar@gmail.com>
2025-03-19 11:31:09 +0530

    corrected the import

--- jobPortal_Client/src/pages/Home.jsx
+++ jobPortal_Client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import JobListing from '../components/jobListing'
 import AppDownload from '../components/AppDownload'
 import Footer from '../components/Footer'
+import JobListing from '../components/jobListing'
+
 
 
 const Home = () => {
