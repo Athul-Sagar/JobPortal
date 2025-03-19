49b73ab1b1f574571832d5edb865d5c693f80329
Athul-Sagar <athulsagar@gmail.com>
2025-03-19 11:39:17 +0530

    corrected

--- jobPortal_Client/src/pages/Home.jsx
+++ jobPortal_Client/src/pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import AppDownload from '../components/AppDownload'
-import Footer from '../components/Footer'
-import JobListing from '../components/jobListing'
+import Footer from '../components/Footer.jsx'
+import JobListing from '../components/JobListing'
+
 
 
 
